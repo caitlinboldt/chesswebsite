22c36f8b20808f0f480821b8d0425251a2560bdb
caitlinboldt <caitlin@boldt.ca>
2018-02-26 13:07:18 -0800

    update

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//first name
+//last name
+// age in years
+// alert 1-greeting 2-full name 3- age in months 4- age in days
+
+// var firstName = prompt('What is your first name?');
+// var lastName = prompt('What is your last name?');
+// var ageYear = prompt('How old are you?');
+
+// var ageMonth = ageYear * 12
+// var ageDays = ageYear * 365
+
+// alert(`Hello and welcome to the site ${firstName} ${lastName}. Your age in months is ${ageMonth} and in days is ${ageDays}`);
