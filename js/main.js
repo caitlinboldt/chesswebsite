bae8d20ea43a0f20560a7ed93bdae3687d41de7e
caitlinboldt <caitlin@boldt.ca>
2018-03-06 15:25:25 -0800

    update

--- js/main.js
+++ js/main.js
@@ -11,3 +11,49 @@
 // var ageDays = ageYear * 365
 
 // alert(`Hello and welcome to the site ${firstName} ${lastName}. Your age in months is ${ageMonth} and in days is ${ageDays}`);
+
+
+	/*------------------
+		CONTACT FORM
+	--------------------*/
+	$('#contact-form').on('submit', function() {
+		var send_btn = $('#send-form'),
+			form = $(this),
+			formdata = $(this).serialize(),
+			chack = $('#form-chack');
+			send_btn.text('Wait...');
+
+		function reset_form(){
+		 	$("#name").val('');
+			$("#email").val('');
+			$("#massage").val('');
+		}
+
+		$.ajax({
+			url:  $(form).attr('action'),
+			type: 'POST',
+			data: formdata,
+			success : function(text){
+				if (text == "success"){
+					send_btn.addClass('done');
+					send_btn.text('Success');
+					setTimeout(function() {
+						reset_form();
+						send_btn.removeClass('done');
+						send_btn.text('Send Massage');
+					}, 3000);
+				}
+				else {
+					reset_form();
+					send_btn.addClass('error');
+					send_btn.text('Error');
+					setTimeout(function() {
+						send_btn.removeClass('error');
+						send_btn.text('Send Massage');
+					}, 5000);
+				}
+			}
+		});
+		return false;
+	});
+
